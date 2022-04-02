23f46dafbe9b7a83cc944d7fd2fdc7e199145092
mgibso22 <77817564+mgibso22@users.noreply.github.com>
2022-04-02 18:51:16 -0500

    Add files via upload

---
+++ sql/deleteDB.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS force_type;
+DROP TABLE IF EXISTS infractions;
+DROP TABLE IF EXISTS police_officers;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS victims;
---
+++ sql/queries.sql
@@ -0,0 +1,42 @@
+SELECT *
+FROM police_officers
+WHERE (department = 'Chicago')
+ORDER BY officer_rank;
+
+SELECT *
+FROM victims
+WHERE (gender = 'male')
+ORDER BY victim_name;
+
+
+
+SELECT LOWER(address)
+FROM victims;
+
+SELECT COUNT(*)
+FROM infractions
+WHERE (date = CURDATE());
+
+SELECT force_type
+FROM infractions
+GROUP BY force_type
+HAVING COUNT(*) > 3;
+
+
+
+SELECT *
+FROM infractions
+WHERE EXISTS
+(SELECT victim_id FROM victims WHERE address IS NOT NULL);
+
+SELECT *
+FROM INFRACTIONS
+INNER JOIN victims ON (infraction.victim = victims.victim_id);
+
+SELECT COUNT(*)
+FROM (
+    SELECT COUNT(*) AS NUM 
+    FROM infractions
+    GROUP BY force_type
+)
+WHERE NUM >= 2;
